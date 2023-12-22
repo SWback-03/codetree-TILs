895018422047e5e80cca80a12b0c5996c942be64
WON <137044064+WON-0303@users.noreply.github.com>
2023-12-23 00:26:39 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a=30.48;
+    double b=160934;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<9.2<<"ft = "<<9.2*a<<"cm"<<endl;
+    cout<<1.3<<"mi = "<<1.3*b<<"cm";
+    return 0;
+}
