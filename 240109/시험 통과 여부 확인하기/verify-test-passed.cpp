9b00d5640f349176ffc801bf83163f7824e9292a
WON <137044064+WON-0303@users.noreply.github.com>
2024-01-09 20:57:27 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    if(n>=80){
+        cout<<"pass";
+    }
+    else{
+        int k = 80-n;
+        cout<<"\""<<k<<" more score\"";
+    }
+    return 0;
+}
