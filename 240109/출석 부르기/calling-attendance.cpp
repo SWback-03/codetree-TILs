16a4e7fead0f43532b1fd93b31dddc4de9b5d157
WON <137044064+WON-0303@users.noreply.github.com>
2024-01-09 21:04:31 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    if(n==1){
+        cout<<"John";
+    }
+    else if(n==1){
+        cout<<"Tom";
+    }
+    else if(n==1){
+        cout<<"Paul";
+    }
+    else{
+        cout<<"Vacancy";
+    }
+    return 0;
+}
