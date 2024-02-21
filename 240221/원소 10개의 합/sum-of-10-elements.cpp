a17acebe343c1b03fd5107116c6260b9b0e5ef09
WON <137044064+WON-0303@users.noreply.github.com>
2024-02-21 13:37:50 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240221/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum=0, num;
+    for(int i=0; i<10; i++){
+        cin>>num;
+        sum+=num;
+    }
+    cout<<sum;
+    return 0;
+}
