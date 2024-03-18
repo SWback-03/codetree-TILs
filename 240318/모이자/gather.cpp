dcd91413a6319cd317a44a7cceb913b22026cf1d
WON <137044064+WON-0303@users.noreply.github.com>
2024-03-18 21:59:21 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/모이자/gather.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int calculate(vector<int> a, int n){
+
+    int min_value = 9999;
+
+    for(int i=0; i<n; ++i){
+        int sum = 0;
+        for(int j=0; j<n; ++j){
+            if(i==j) continue;
+            sum += (abs(i-j))*a[j];
+        }
+        if(min_value>=sum) min_value = sum;
+    }
+    return min_value;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    vector<int> vect;
+    for(int i=0; i<n; ++i){
+        int person;
+        cin>>person;
+        vect.push_back(person);
+    }
+    cout<<calculate(vect,n);
+
+    return 0;
+}
