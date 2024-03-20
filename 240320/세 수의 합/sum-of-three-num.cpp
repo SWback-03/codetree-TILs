a79b5ebad398a210de7d4c72d8c07548e0635714
WON <137044064+WON-0303@users.noreply.github.com>
2024-03-20 20:25:30 +0900

    Title: 세 수의 합, Time: 178ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240320/세 수의 합/sum-of-three-num.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    // 배열 정렬
+    sort(arr.begin(), arr.end());
+
+    int ans = 0;
+    // 세 수의 조합을 찾기
+    for(int i = 0; i < n; ++i) {
+        for(int j = i + 1; j < n; ++j) {
+            for(int l = j + 1; l < n; ++l) {
+                if(arr[i] + arr[j] + arr[l] == k) {
+                    ans++;
+                }
+            }
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
