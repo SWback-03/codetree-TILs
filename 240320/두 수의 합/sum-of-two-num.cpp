9b9e3de95d96ad5499b3a7a4a92f76a59728cf53
WON <137044064+WON-0303@users.noreply.github.com>
2024-03-20 18:59:09 +0900

    Title: 두 수의 합, Time: 1092ms, Memory: 5MB, Status: Time Limit Exceed - CodeTree

---
+++ 240320/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, k;
+    cin>>n>>k;
+
+    vector<int> vect(n);
+    unordered_multiset<int> u_m;
+
+    for(int i=0; i<n; ++i){
+        int input;
+        cin>>input;
+        //q.push(input);
+        vect[i]=input;
+        u_m.insert(input);
+    }
+
+    int result=0;
+    for(int i=0; i<n; ++i){
+        int diff = k-vect[i];
+        u_m.erase(u_m.find(vect[i]));
+        result += u_m.count(diff);
+    }
+    cout<<result;
+
+
+    return 0;
+}
