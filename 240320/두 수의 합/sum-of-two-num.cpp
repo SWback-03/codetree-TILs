a2c871091730e3cbe3c38a8ffd94e9d36847bbc3
WON <137044064+WON-0303@users.noreply.github.com>
2024-03-20 19:03:37 +0900

    Title: 두 수의 합, Time: 82ms, Memory: 3MB, Status: Passed - CodeTree

--- 240320/두 수의 합/sum-of-two-num.cpp
+++ 240320/두 수의 합/sum-of-two-num.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
-#include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-
-
 int main() {
-    // 여기에 코드를 작성해주세요.
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
     int n, k;
-    cin>>n>>k;
+    cin >> n >> k;
 
     vector<int> vect(n);
-    unordered_multiset<int> u_m;
+    unordered_map<int, int> um; // 각 숫자의 등장 횟수를 저장
 
-    for(int i=0; i<n; ++i){
-        int input;
-        cin>>input;
-        //q.push(input);
-        vect[i]=input;
-        u_m.insert(input);
+    for (int i = 0; i < n; ++i) {
+        cin >> vect[i];
+        um[vect[i]]++; // 숫자의 등장 횟수를 증가
     }
 
-    int result=0;
-    for(int i=0; i<n; ++i){
-        int diff = k-vect[i];
-        u_m.erase(u_m.find(vect[i]));
-        result += u_m.count(diff);
+    int result = 0;
+    for (int i = 0; i < n; ++i) {
+        int diff = k - vect[i];
+        if (um.find(diff) != um.end()) { // diff가 um에 존재하는 경우
+            result += um[diff]; // diff의 등장 횟수를 결과에 추가
+            // 자기 자신을 차이값으로 가지는 경우, 그 횟수를 1 감소시킴
+            if (diff == vect[i]) {
+                result--;
+            }
+        }
     }
-    cout<<result;
 
+    // 결과를 2로 나눔 (각 쌍이 두 번씩 세어졌기 때문)
+    cout << result / 2 << endl;
 
     return 0;
 }
