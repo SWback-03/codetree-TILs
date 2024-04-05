d726fb43f57f21229a50da6b3b6fb306908beed9
WON <137044064+WON-0303@users.noreply.github.com>
2024-04-05 21:35:35 +0900

    Title: 중앙값, Time: 1098ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240405/중앙값/median.cpp
+++ 240405/중앙값/median.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+priority_queue<int> pq;
+priority_queue<int> temp;
 vector<int> v;
-
 int t, m;
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin>>t;
     for(int i=0; i<t; ++i){
-        v = vector<int>();
+        pq = priority_queue<int>();
         cin>>m;
         for(int j=0; j<m; ++j)
         {
             int tmp;
             cin>>tmp;
-            v.push_back(tmp);
-            sort(v.begin(),v.end());
-            if(j%2==0){
-                cout<<v[j/2]<<" ";
+            pq.push(tmp);
+            temp = pq;
+            if(!(j%2)){ //홀수일 때
+                int cnt = 0;
+                while(1){
+                    if(cnt == j/2){
+                        cout<<temp.top()<<" ";
+                        break;
+                    }
+                    cnt++;
+                    temp.pop();
+                    
+                }
             }
         }
         cout<<"\n";
