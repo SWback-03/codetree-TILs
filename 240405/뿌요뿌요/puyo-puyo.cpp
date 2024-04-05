26f2c2cdbcabdd235347b8fdb82a4c44094d445d
WON <137044064+WON-0303@users.noreply.github.com>
2024-04-05 23:06:53 +0900

    Title: 뿌요뿌요, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240405/뿌요뿌요/puyo-puyo.cpp
+++ 240405/뿌요뿌요/puyo-puyo.cpp
@@ -1,81 +1,65 @@
 #include <iostream>
 #include <queue>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
 int n;
-int dx[4] = {-1,1,0,0};
-int dy[4] = {0,0,-1,1};
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 int map[102][102] = {0,};
-int visited[102][102]= {0,};
-int bomb[102][102]= {0,};
-int bomb_temp[102][102]= {0,};
+int visited[102][102] = {0,};
+int bomb[102][102] = {0,};
 
 int bomb_cnt = 0;
 
-stack<pair<int,int>> q;
-
-int bfs(int sy, int sx){
-    q.push({sy,sx});
+int bfs(int sy, int sx) {
+    queue<pair<int, int>> q;
+    q.push({sy, sx});
     int val = map[sy][sx];
     int cnt = 1;
-    
-    
-    while(!q.empty()){
-        int y = q.top().first;
-        int x = q.top().second;
-        visited[y][x] = 1;
-        bomb[y][x] = 1;
+    visited[sy][sx] = 1;
+
+    while (!q.empty()) {
+        int y = q.front().first;
+        int x = q.front().second;
         q.pop();
 
-        for(int i=0; i<4; ++i){
-            int ny = y+dy[i];
-            int nx = x+dx[i];
-            if(map[ny][nx] > 0 && !visited[ny][nx] && map[ny][nx] == val){
-                q.push({ny,nx});
+        for (int i = 0; i < 4; ++i) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny > 0 && ny <= n && nx > 0 && nx <= n && map[ny][nx] == val && !visited[ny][nx]) {
+                visited[ny][nx] = 1;
+                q.push({ny, nx});
                 cnt++;
             }
         }
     }
-    if(cnt<4){
-        bomb[102][102] = bomb_temp[102][102]; //이전으로 되돌림
-    }
-    else {
-        bomb_temp[102][102] = bomb[102][102]; //bomb 업데이트
-        bomb_cnt++;
-    }
-
-    return cnt; //이어서 터진 폭탄 갯수
+    return cnt >= 4 ? cnt : 0; // 터질 경우 해당 블럭 크기 반환, 아닐 경우 0 반환
 }
 
 int main() {
-    cin>>n;
-    for(int i=1; i<=n; ++i){
-        for(int j=1; j<=n; ++j){
-            cin>>map[i][j];
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin >> map[i][j];
         }
     }
 
-    int max = 0;
-    for(int i=1; i<=n; ++i){
-        for(int j=1; j<=n; ++j){
-            if(visited[i][j]==0) {
-                int result = bfs(i,j);
-                if(max<result) max = result;
+    int max_block = 0, total_bombs = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (!visited[i][j]) {
+                int result = bfs(i, j);
+                if (result > 0) {
+                    total_bombs++;
+                    if (result > max_block) max_block = result;
+                }
             }
-            
         }
     }
 
-    int cnt = 0;
-    for(int i=1; i<=n; ++i){
-        for(int j=1; j<=n; ++j){
-            if(bomb[i][j] == 1) cnt++;
-            
-        }
-    }
-    cout<<bomb_cnt<<" "<<max;
+    cout << total_bombs << " " << max_block;
 
     return 0;
 }
