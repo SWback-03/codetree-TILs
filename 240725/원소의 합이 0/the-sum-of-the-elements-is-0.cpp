c4938cd8dcdc47bd0410583dfcff7d9b751c8a5c
Sangwon Back <137044064+SWback-03@users.noreply.github.com>
2024-07-25 15:18:38 +0900

    Title: 원소의 합이 0, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+#define max_n 5000
+
+int arr[4][max_n] = {0};
+
+unordered_map<int,int> map;
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin>>n;
+
+    for(int i=0; i<4; ++i){
+        for(int j=0; j<n; ++j){
+            int tmp;
+            cin>>tmp;
+            arr[i][j] = tmp;
+        }
+    }
+    for(int i =0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            map[arr[0][i]+arr[1][j]]++;
+        }
+    }
+
+    int count = 0;
+
+    for(int i =0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            if(map.find(-(arr[2][i]+arr[3][j])) != map.end()){
+                count++;
+            }
+        }
+    }
+
+    cout<<count;
+    
+    return 0;
+}
