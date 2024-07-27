c0e4d3eea36dfb0db5fcd601fec1328e69368bb3
Sangwon Back <137044064+SWback-03@users.noreply.github.com>
2024-07-27 18:23:51 +0900

    Title: k번째로 작은 쌍의 합, Time: 104ms, Memory: 2MB, Status: Passed - CodeTree

--- 240727/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
+++ 240727/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <unordered_set>
+#include <algorithm>
+
 using namespace std;
 
-#define MAX_N 100000
-#define MAX_M 100000
-
-int n, m, k;
-
-priority_queue<int> arr;
-unordered_multiset<int> umset;
-
 int main() {
+    int n, m, k;
     cin >> n >> m >> k;
 
+    vector<int> arr1(n);
+    vector<int> arr2(m);
+
     for (int i = 0; i < n; ++i) {
-        int tmp;
-        cin >> tmp;
-        umset.insert(tmp);
+        cin >> arr1[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> arr2[i];
     }
 
-    for (int i = 0; i < m; ++i) {
-        int tmp;
-        cin >> tmp;
-        for (auto iter : umset) {
-            int sum = iter + tmp;
-            if (arr.size() < k) {
-                arr.push(sum);
-            } else if (sum < arr.top()) {
-                arr.pop();
-                arr.push(sum);
-            }
+    // 정렬
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+
+    // 최소 힙
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> min_heap;
+
+    // 최소 힙 초기화
+    for (int i = 0; i < n; ++i) {
+        min_heap.push({arr1[i] + arr2[0], {i, 0}});
+    }
+
+    // k번째 작은 합을 찾기
+    int result = 0;
+    for (int count = 0; count < k; ++count) {
+        auto current = min_heap.top();
+        min_heap.pop();
+        result = current.first;
+        int i = current.second.first;
+        int j = current.second.second;
+        if (j + 1 < m) {
+            min_heap.push({arr1[i] + arr2[j + 1], {i, j + 1}});
         }
     }
 
-    cout << arr.top() << endl;
+    cout << result << endl;
 
     return 0;
 }
