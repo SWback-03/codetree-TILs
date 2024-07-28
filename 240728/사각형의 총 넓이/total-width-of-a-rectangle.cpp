7affebcebb23bda4fec3eb2bcb0bbea5cbab502b
Sangwon Back <137044064+SWback-03@users.noreply.github.com>
2024-07-28 19:04:00 +0900

    Title: 사각형의 총 넓이, Time: 901ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 240728/사각형의 총 넓이/total-width-of-a-rectangle.cpp
+++ 240728/사각형의 총 넓이/total-width-of-a-rectangle.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-#define max_point 10001
+#define max_point 20001
 
-int arr[max_point][max_point] = {0};
+bool arr[max_point][max_point] = {false};
 
 int main() {
     int N;
@@ -12,9 +12,13 @@ int main() {
     for(int i=0; i<N; ++i){
         int x1, y1, x2, y2;
         cin>>x1>>y1>>x2>>y2;
+        x1 = x1+10000;
+        y1 = y1+10000;
+        x2 = x2+10000;
+        y2 = y2+10000;
         for(int x = x1; x<x2; ++x){
             for(int y = y2; y<y1; ++y){
-                arr[y][x] = 1;
+                arr[y][x] = true;
             }
         }
     }
@@ -23,7 +27,7 @@ int main() {
 
     for(int i=0; i<max_point; ++i){
         for(int j=0; j<max_point; ++j){
-            if(arr[i][j]>0){
+            if(arr[i][j]==true){
                 count++;
                 
             }
